6f2456a3df1c2ca1d0098e8c101bb7314b4b09d1
Kazuto <mail@kazuto.de>
2023-08-10 08:01:57 +0200

    feat(core): add user development packages

--- nixos/home.nix
+++ nixos/home.nix
@@ -18,6 +18,55 @@
       spotify
       teams
       telegram-desktop
+
+      # Development
+      beekeper-studio
+      codespell
+      commitizen
+      dbeaver
+      docker
+      docker-compose
+      editorconfig-checker
+      fd
+      gh
+      gitkraken
+      insomnia
+      jetbrains.phpstorm
+      kitty
+      neovim
+      nodejs_20
+      nodePackages.eslint_d
+      nodePackages.npm
+      nodePackages.postcss
+      nodePackages.prettier_d_slim
+      nodePackages.vercel
+      nodePackages.volar
+      nodePackages.yarn
+      oh-my-zsh
+
+      (php.buildEnv {
+        extensions = ({ enabled, all }: enabled ++ (with all; [
+          xdebug imagick
+        ]));
+        extraConfig = ''
+          memory_limit = 8G
+          IDE Key = nvim
+          xdebug.mode = debug
+        '';
+      })
+
+      php81Packages.composer
+      php81Extensions.imagick
+      postman
+      pre-commit
+      shellcheck
+      skeema
+      supabase-cli
+      trash-cli
+      tree
+      vimPlugins.codeium-vim
+      vscode
+      zsh-powerlevel10k
     ];
   };
 
