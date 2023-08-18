6578d56883c81e8c8878044823bc8baa0c9ddd28
Kazuto <mail@kazuto.de>
2023-08-18 07:39:41 +0200

    fix(network): remove dhcp = "internal"

--- modules/hardware/network/default.nix
+++ modules/hardware/network/default.nix
@@ -13,7 +13,6 @@ in
     networking = {
       networkmanager = {
         enable = true;
-        dhcp = "internal";
       };
     };
 
@@ -21,7 +20,7 @@ in
 
     # Fixes an issue that normally causes nixos-rebuild to fail.
     # https://github.com/NixOS/nixpkgs/issues/180175
-    systemd.services.NetworkManager-wait-online.enable = false;
+    # systemd.services.NetworkManager-wait-online.enable = false;
   };
 }
 
