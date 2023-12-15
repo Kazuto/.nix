f0dd26c6bdb76006245a479756b041e477556bf0
Kazuto <mail@kazuto.de>
2023-12-15 10:41:59 +0100

    fix(neovim): buffer next tab keymap

--- modules/darwin/cli/neovim/config/lua/kazuto/core/keymaps.lua
+++ modules/darwin/cli/neovim/config/lua/kazuto/core/keymaps.lua
@@ -61,5 +61,5 @@ Map('v', '<K>', ":m '<-2<CR>gv=gv")
 
 -- bufferline
 Map("n", "<Tab>", ":BufferLineCycleNext<CR>", { desc = "Next Buffer" })
-Map("n", "<S-Tab>", ":BufferLineCyclePrevious<CR>", { desc = "Previous Buffer" })
+Map("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { desc = "Previous Buffer" })
 Map("n", "<C-x>", ":bdelete<CR>", { desc = "Close Buffer" })
