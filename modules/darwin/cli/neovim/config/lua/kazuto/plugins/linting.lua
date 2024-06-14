4ad2cd1908d2c548683ac0509a0459fd526ee34a
Kazuto <mail@kazuto.de>
2024-06-14 13:10:18 +0200

    feat(nvim): disable lint on buf change

--- modules/darwin/cli/neovim/config/lua/kazuto/plugins/linting.lua
+++ modules/darwin/cli/neovim/config/lua/kazuto/plugins/linting.lua
@@ -1,6 +1,6 @@
 return {
 	"mfussenegger/nvim-lint",
-	event = { "BufReadPre", "BufNewFile" },
+	-- event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		local lint = require("lint")
 
@@ -12,14 +12,14 @@ return {
 			vue = { "eslint_d" },
 		}
 
-		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		-- local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			group = lint_augroup,
-			callback = function()
-				lint.try_lint()
-			end,
-		})
+		-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+		-- 	group = lint_augroup,
+		-- 	callback = function()
+		-- 		lint.try_lint()
+		-- 	end,
+		-- })
 
 		vim.keymap.set("n", "<leader>l", function()
 			lint.try_lint()
