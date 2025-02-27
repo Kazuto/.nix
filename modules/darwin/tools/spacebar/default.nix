22de68d0795a89cf24982c40494254469d3683d8
Kazuto <mail@kazuto.de>
2025-02-27 10:10:57 +0100

    feat(darwin): add nowplaying-cli to spacebar

--- modules/darwin/tools/spacebar/default.nix
+++ modules/darwin/tools/spacebar/default.nix
@@ -11,7 +11,7 @@ in
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ spacebar ];
+    environment.systemPackages = with pkgs; [ spacebar nowplaying-cli ];
 
     shiro.home = {
       file = {
