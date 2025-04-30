90a067c393c131d58d1bf2d0cf8e46919ca18504
Kazuto <mail@kazuto.de>
2025-04-30 11:15:30 +0200

    refactor(darwin): width and state in media playback sketchybar item

--- .dotfiles/.config/sketchybar/plugins/media.sh
+++ .dotfiles/.config/sketchybar/plugins/media.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 STATE="$(echo "$INFO" | jq -r '.state')"
+MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
 if [ "$STATE" = "playing" ]; then
-  MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
   sketchybar --set $NAME label="$MEDIA" drawing=on
 else
-  sketchybar --set $NAME drawing=off
+  sketchybar --set $NAME label="$MEDIA [Paused]" drawing=on
 fi
