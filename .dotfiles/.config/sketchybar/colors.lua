7187213b01c7534914f4736786d160d81c2e9a6d
Kazuto <mail@kazuto.de>
2025-07-11 10:39:28 +0200

    wip(darwin): convert sketchybar to lua config

---
+++ .dotfiles/.config/sketchybar/colors.lua
@@ -0,0 +1,41 @@
+return {
+	bar = {
+		bg = "0xff1e1e2e",
+	},
+	item = {
+		bg = "0xff181825",
+		text = "0xffcdd6f4",
+	},
+
+	-- Catppuccin Mocha Palette
+	catppuccin = {
+		mocha = {
+			rosewater = "0Xfff5e0dc",
+			flamingo = "0Xfff2cdcd",
+			pink = "0Xfff5c2e7",
+			mauve = "0Xffcba6f7",
+			red = "0Xfff38ba8",
+			maroon = "0Xffeba0ac",
+			peach = "0Xfffab387",
+			yellow = "0Xfff9e2af",
+			green = "0Xffa6e3a0",
+			teal = "0Xff94e2d5",
+			sky = "0Xff89dceb",
+			sapphire = "0Xff74c7ec",
+			blue = "0Xff89b4fa",
+			lavender = "0Xffb4befe",
+			text = "0Xffcdd6f4",
+			subtext1 = "0Xffbac2de",
+			subtext0 = "0Xffa6adc8",
+			overlay2 = "0Xff9399b2",
+			overlay1 = "0Xff7f849c",
+			overlay0 = "0Xff6c7086",
+			surface2 = "0Xff585b70",
+			surface1 = "0Xff45475a",
+			surface0 = "0Xff313244",
+			base = "0Xff1e1e2e",
+			mantle = "0Xff181825",
+			crust = "0Xff11111b",
+		},
+	},
+}
