5723b7b3116c938364ed779b96f5c1db2027408d
Kazuto <mail@kazuto.de>
2025-07-15 10:08:44 +0200

    fix: indent

--- .dotfiles/.config/sketchybar/bar.lua
+++ .dotfiles/.config/sketchybar/bar.lua
@@ -2,13 +2,13 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-	height = 42,
-	display = "main",
-	blur_radius = 0,
-	position = "top",
-	sticky = false,
-	corner_radius = 10,
-	margin = 10,
-	y_offset = 4,
-	color = colors.bar.bg,
+  height = 42,
+  display = "main",
+  blur_radius = 0,
+  position = "top",
+  sticky = false,
+  corner_radius = 10,
+  margin = 10,
+  y_offset = 4,
+  color = colors.bar.bg,
 })
