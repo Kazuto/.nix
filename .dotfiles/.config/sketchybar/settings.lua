2013dd917a8d144e684e59a7220b1e531ae395ae
Kazuto <mail@kazuto.de>
2025-07-15 10:08:44 +0200

    fix: indent

--- .dotfiles/.config/sketchybar/settings.lua
+++ .dotfiles/.config/sketchybar/settings.lua
@@ -1,13 +1,13 @@
 return {
-	label = {
-		font = "SF Pro",
-		style = "Medium",
-		size = 13.0,
-	},
-	icon = {
-		font = "JetBrainsMono Nerd Font Propo",
-		style = "Semibold",
-		size = 13.0,
-	},
-	paddings = 6,
+  label = {
+    font = "SF Pro",
+    style = "Medium",
+    size = 13.0,
+  },
+  icon = {
+    font = "JetBrainsMono Nerd Font Propo",
+    style = "Semibold",
+    size = 13.0,
+  },
+  paddings = 6,
 }
