fadb02e9014c3d423c5857d498e243bc4f31a8e7
Kazuto <mail@kazuto.de>
2025-07-29 12:44:47 +0200

    fix(darwin): sketchybar spotify item

--- .dotfiles/.config/sketchybar/items/spotify.sh
+++ .dotfiles/.config/sketchybar/items/spotify.sh
@@ -15,18 +15,19 @@ sketchybar --add event spotify_change $SPOTIFY_EVENT \
   popup.background.corner_radius=10 \
   popup.background.color="$CAT_BASE" \
   popup.background.border_width=1 \
-  popup.background.border_color="$CAT_SURFACE1"
-
-sketchybar --add item spotify.back popup.spotify.name \
+  popup.background.border_color="$CAT_SURFACE1" \
+  \
+  --add item spotify.back popup.spotify.name \
   --set spotify.back icon=􀊎 \
+  padding_left=16 \
   icon.padding_left=5 \
   icon.padding_right=5 \
   script="$PLUGIN_DIR/spotify.sh" \
   label.drawing=off \
   background.drawing=off \
-  --subscribe spotify.back mouse.clicked
-
-sketchybar --add item spotify.play popup.spotify.name \
+  --subscribe spotify.back mouse.clicked \
+  \
+  --add item spotify.play popup.spotify.name \
   --set spotify.play icon=􀊔 \
   icon.padding_left=5 \
   icon.padding_right=5 \
@@ -34,18 +35,18 @@ sketchybar --add item spotify.play popup.spotify.name \
   label.drawing=off \
   background.drawing=off \
   script="$PLUGIN_DIR/spotify.sh" \
-  --subscribe spotify.back mouse.clicked
-
-sketchybar --add item spotify.next popup.spotify.name \
+  --subscribe spotify.play mouse.clicked spotify_change \
+  \
+  --add item spotify.next popup.spotify.name \
   --set spotify.next icon=􀊐 \
   icon.padding_left=5 \
   icon.padding_right=10 \
   label.drawing=off \
   background.drawing=off \
   script="$PLUGIN_DIR/spotify.sh" \
-  --subscribe spotify.back mouse.clicked
-
-sketchybar --add item spotify.shuffle popup.spotify.name \
+  --subscribe spotify.next mouse.clicked \
+  \
+  --add item spotify.shuffle popup.spotify.name \
   --set spotify.shuffle icon=􀊝 \
   icon.highlight_color=0xff1DB954 \
   icon.padding_left=5 \
@@ -53,13 +54,14 @@ sketchybar --add item spotify.shuffle popup.spotify.name \
   label.drawing=off \
   background.drawing=off \
   script="$PLUGIN_DIR/spotify.sh" \
-  --subscribe spotify.back mouse.clicked
-
-sketchybar --add item spotify.repeat popup.spotify.name \
+  --subscribe spotify.shuffle mouse.clicked \
+  \
+  --add item spotify.repeat popup.spotify.name \
   --set spotify.repeat icon=􀊞 \
   icon.highlight_color=0xff1DB954 \
   icon.padding_left=5 \
   icon.padding_right=5 \
+  padding_right=16 \
   label.drawing=off \
   background.drawing=off \
   script="$PLUGIN_DIR/spotify.sh" \
