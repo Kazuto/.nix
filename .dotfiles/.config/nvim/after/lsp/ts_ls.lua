9640b3edd13c889ce9522cf604c39e5dab67e97f
Kazuto <mail@kazuto.de>
2026-02-26 10:15:14 +0100

    refactor(LSP): Configure TypeScript and Vue language servers for improved Vue support
    - Remove separate eslint.lua LSP configuration file
    - Add Vue TypeScript plugin support to ts_ls.lua
    - Enable hybrid mode in vue_ls.lua
    - Add nvim-lint plugin with ESLint integration for multiple filetypes including Vue
    - Update lazy-lock.json with nvim-lint dependency

--- .dotfiles/.config/nvim/after/lsp/ts_ls.lua
+++ .dotfiles/.config/nvim/after/lsp/ts_ls.lua
@@ -1,3 +1,7 @@
+local vue_plugin_path = vim.fn.expand(
+  "$MASON/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin"
+)
+
 return {
   filetypes = {
     "javascript",
@@ -6,5 +10,15 @@ return {
     "typescript",
     "typescriptreact",
     "typescript.tsx",
+    "vue",
+  },
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vue_plugin_path,
+        languages = { "vue" },
+      },
+    },
   },
 }
