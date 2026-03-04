46c14e7fe962de0c6f28e9a6c862d940be60e24c
Kazuto <mail@kazuto.de>
2026-03-04 12:50:05 +0100

    fix waybar

--- modules/nixos/desktop/addons/waybar/default.nix
+++ modules/nixos/desktop/addons/waybar/default.nix
@@ -13,19 +13,7 @@ in
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [ playerctl inotify-tools];
 
-    programs.waybar = {
-      enable = true;
-      package = pkgs.waybar.overrideAttrs (oa: {
-        mesonFlags = oa.mesonFlags ++ [ "-Dexperimental=true" ];
-        patches = (oa.patches or []) ++ [
-          (pkgs.fetchpatch {
-            name = "fix waybar hyprctl";
-            url = "https://aur.archlinux.org/cgit/aur.git/plain/hyprctl.patch?h=waybar-hyprland-git";
-            sha256 = "sha256-pY3+9Dhi61Jo2cPnBdmn3NUTSA8bAbtgsk2ooj4y7aQ=";
-          })
-        ];
-      });
-    };
+    programs.waybar.enable = true;
 
     shiro.home.configFile."waybar".source = ./config;
   };
