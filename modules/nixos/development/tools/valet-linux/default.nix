00bbdcb25e5cbfc026029e7880b960825349f79e
Kazuto <mail@kazuto.de>
2026-03-06 20:28:19 +0100

    fix: valet

--- modules/nixos/development/tools/valet-linux/default.nix
+++ modules/nixos/development/tools/valet-linux/default.nix
@@ -1,18 +1,19 @@
-{ options, config, lib, pkgs, ... }:
-
-with lib;
-with lib.shiro;
-let
-  cfg = config.shiro.development.tools.valet-linux;
-in
 {
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.shiro; let
+  cfg = config.shiro.development.tools.valet-linux;
+in {
   options.shiro.development.tools.valet-linux = with types; {
     enable = mkBoolOpt false "Whether or not to install valet-linux.";
   };
 
   config = mkIf cfg.enable {
-    shiro.development.languages.php8 = enabled;
-
     services.nginx.enable = true;
 
     services.mysql = {
