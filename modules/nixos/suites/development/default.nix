853daca5a69c00f145db46e2c168204e00eb2ad2
Kazuto <mail@kazuto.de>
2026-03-06 20:27:31 +0100

    fix: nixos

--- modules/nixos/suites/development/default.nix
+++ modules/nixos/suites/development/default.nix
@@ -1,20 +1,22 @@
-{ options, config, lib, pkgs, ... }:
-
-with lib;
-with lib.shiro;
-let
-  cfg = config.shiro.suites.development;
-in
 {
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.shiro; let
+  cfg = config.shiro.suites.development;
+in {
   options.shiro.suites.development = with types; {
     enable = mkBoolOpt false "Whether or not to enable development configuration.";
   };
 
   config = mkIf cfg.enable {
     shiro = {
-      cli = {
-        bat = enabled;
-        curl = enabled;
+      development.tools = {
+        valet-linux = enabled;
       };
     };
   };
