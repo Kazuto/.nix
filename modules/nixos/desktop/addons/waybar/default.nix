d7f2f8df80761d9d1d5cd26948783dd56a18ee00
Kazuto <mail@kazuto.de>
2026-03-06 21:24:55 +0100

    fix: waybar

--- modules/nixos/desktop/addons/waybar/default.nix
+++ modules/nixos/desktop/addons/waybar/default.nix
@@ -20,14 +20,28 @@ in
 
     programs.waybar.enable = true;
 
-    shiro.home.configFile."waybar".source = ./config;
+    shiro.home.configFile."waybar/config.jsonc".source = ./config/config.jsonc;
+    shiro.home.configFile."waybar/modules.jsonc".source = ./config/modules.jsonc;
+    shiro.home.configFile."waybar/style.css".source = ./config/style.css;
 
     # Make scripts executable
     shiro.home.extraOptions = {
-      home.file.".config/waybar/scripts/github".executable = true;
-      home.file.".config/waybar/scripts/github-menu".executable = true;
-      home.file.".config/waybar/scripts/timer".executable = true;
-      home.file.".config/waybar/scripts/logitech".executable = true;
+      home.file.".config/waybar/scripts/github" = {
+        source = ./config/scripts/github;
+        executable = true;
+      };
+      home.file.".config/waybar/scripts/github-menu" = {
+        source = ./config/scripts/github-menu;
+        executable = true;
+      };
+      home.file.".config/waybar/scripts/timer" = {
+        source = ./config/scripts/timer;
+        executable = true;
+      };
+      home.file.".config/waybar/scripts/logitech" = {
+        source = ./config/scripts/logitech;
+        executable = true;
+      };
     };
   };
 }
